413e208d4bb11280618899778589e7c0ec2f8cc3
Justin S. Kim <skim152@student.umuc.edu>
2017-09-11 17:52:37 -0400

    Add files via upload

---
+++ ApplesCountShinyeobKim.java
@@ -0,0 +1,11 @@
+public class ApplesCountShinyeobKim {
+    public static void main(String[] args){
+         int[] count =
+        {3,22,3,14,54,36,27,38,29,13,2,42,85,1,2}; //Counts for each batch of apples.
+         for (int apples : count) {
+             System.out.println("Apple count is: " + apples); //shows apple count.
+         }
+    }
+}
+
+
